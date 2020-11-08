fbf5ab28aaac1e7df3f8526b99a9731eee8388b4
kolodidon <www.teteva@yandex.ru>
2020-11-08 13:16:10 +0300

    19. Setted up routing, created <News/> <Music/> <Settings/>

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,11 +5,11 @@ const Sidebar = () => {
     return (
         <aside className={s.nav}>
             <nav>
-                <a href="#1">Profile</a>
-                <a href="#2">Messages</a>
-                <a href="#3">News</a>
-                <a href="#4">Music</a>
-                <a href="#5">Settings</a>
+                <a href="/">Profile</a>
+                <a href="/dialogs">Dialogs</a>
+                <a href="/news">News</a>
+                <a href="/music">Music</a>
+                <a href="/settings">Settings</a>
             </nav>
         </aside>
     )
