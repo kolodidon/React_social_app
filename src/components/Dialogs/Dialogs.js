741a8fe656e9f2078c1e6dc33e5ed7c3ea1581a2
kolodidon <www.teteva@yandex.ru>
2020-11-08 20:14:45 +0300

    21. Marked up some structures in <Dialog/>, styles too.

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,29 @@ import s from './Dialogs.module.scss'
 
 const Dialogs = (props) => {
     return (
-        <div style={{backgroundColor: 'red', width: '100%', height: '100%' }}>
-            Dialogs
+        <div className={s.dialogsWrapper}>
+            <div className={s.dialogs}>
+                <div className={s.dialog + ' ' + s.dialogActive}>
+                    Name
+                </div>
+                <div className={s.dialog}>
+                    Name_1
+                </div>
+                <div className={s.dialog}>
+                    Name_2
+                </div>
+                <div className={s.dialog}>
+                    Name_3
+                </div>
+                <div className={s.dialog}>
+                    Name_4
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Wassup 1</div>
+                <div className={s.message}>Wassup 2</div>
+                <div className={s.message}>Wassup 3</div>
+            </div>
         </div>
     )
 }
